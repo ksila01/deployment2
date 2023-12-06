ed1e3e1a9239b3f8cc1d1631bc7bc1f30eca90f3
“Kelvin <ksila04@gmail.com>
2023-12-06 23:33:46 +0300

    streamlit

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-ROM python:3.8
+FROM python:3.8
 
 WORKDIR /app
 
-COPY ./ /app
-COPY ./requirements.txt /app/requirements.txt
-
+COPY . ./
+RUN pip install -r requirements.txt
 EXPOSE 8080
 
 CMD streamlit run --server.port 8080 --server.enableCORS false main.py
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+import tensorflow.keras as keras
+import tensorflow_hub as hub
 
+# Set the background color to light blue
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #add8e6;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Hide Streamlit menu and footer
 hide_streamlit_style = """
 <style>
 #MainMenu {visibility: hidden;}
@@ -10,75 +25,87 @@ footer {visibility: hidden;}
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+# Set page title and description
 st.title('Potato Leaf Disease Prediction')
+with st.sidebar:
+    st.image('img.jpeg')
+    st.title("Kenya's Best Farm App")
+    st.subheader("A one step to detect leaf disease and get evidence-based solutions.")
+st.sidebar.title('Options')
+st.sidebar.markdown('Upload an image and get predictions.')
 
-model = None
-class_names = ["Early Blight", "Late Blight", "Healthy"]
-BUCKET_NAME = "mawaru"
+def get_remedy(disease_class):
+    remedies = {
+        'Potato__Early_blight': " Use copper-based fungicides, practice crop rotation, and ensure proper spacing between plants.",
+        'Potato__Late_blight': " Apply fungicides containing chlorothalonil, practice good garden hygiene, and avoid overhead watering.",
+        'Potato__healthy': "No specific remedy needed for healthy leaves. Maintain proper plant nutrition and good gardening practices.",
+    }
+    return remedies.get(disease_class, "No specific remedy available.")
 
-def download_blob(bucket_name, source_blob_name, destination_file_name):
-    """Downloads a blob from the bucket."""
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
+def main():
+    # File uploader
+    file_uploaded = st.file_uploader('Choose an image...', type='jpg')
 
-    blob.download_to_filename(destination_file_name)
+    if file_uploaded:
+        # Display uploaded image in the first column
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(file_uploaded, caption='Uploaded Image', use_column_width=True)
 
-    print(f"Blob {source_blob_name} downloaded to {destination_file_name}.")
+        # Prediction in the second column
+        with col2:
+            # Prediction
+            result, confidence = predict_class(file_uploaded)
+            st.subheader('Prediction:')
+            st.write(f'Class: {result}')
+            st.write(f'Confidence: {confidence}%')
 
-def predict(request):
-    global model
-    if model is None:
-        download_blob(
-            BUCKET_NAME,
-            "model/potatoes.h5",
-            "/tmp/potatoes.h5",
-        )
-        model = tf.keras.models.load_model("/tmp/potatoes.h5")
+            # Display remedy information
+            remedy = get_remedy(result)
+            st.subheader('Remedy:')
+            st.write(remedy)
 
-    image = Image.open(request).convert("RGB").resize((256, 256))
-    image = np.array(image) / 255
+def predict_class(image):
+    with st.spinner('Loading Model...'):
+        classifier_model = keras.models.load_model(r'potatoes.h5', compile=False)
 
-    img_array = tf.expand_dims(image, 0)
-    predictions = model.predict(img_array)
+    shape = (256, 256, 3)
+    model = keras.Sequential([hub.KerasLayer(classifier_model, input_shape=shape)])
 
-    print("Predictions:", predictions)
+    # Preprocess the image
+    test_image = Image.open(image).resize((256, 256))
+    test_image = keras.preprocessing.image.img_to_array(test_image) / 255.0
+    test_image = np.expand_dims(test_image, axis=0)
 
-    predicted_class = class_names[np.argmax(predictions[0])]
-    confidence = round(100 * np.max(predictions[0]), 2)
+    class_names = ['Potato__Early_blight', 'Potato__Late_blight', 'Potato__healthy']
 
-    return {"class": predicted_class, "confidence": confidence}
+    # Make predictions
+    prediction = model.predict(test_image)
+    confidence = round(100 * np.max(prediction[0]), 2)
+    final_pred = class_names[np.argmax(prediction)]
 
+    return final_pred, confidence
+
+# Footer
 footer = """
 <style>
-a:link , a:visited{
-    color: white;
-    background-color: transparent;
-    text-decoration: None;
-}
-
-a:hover,  a:active {
-    color: red;
-    background-color: transparent;
-    text-decoration: None;
-}
-
 .footer {
     position: fixed;
     left: 0;
     bottom: 0;
     width: 100%;
-    background-color: transparent;
-    color: black;
+    background-color: #f1f1f1;
     text-align: center;
+    padding: 10px;
 }
 </style>
 
 <div class="footer">
-<p align="center"> Developed Kelvin Sila </a></p>
+    <p>Developed by Kelvin Sila</p>
 </div>
 """
 
 st.markdown(footer, unsafe_allow_html=True)
 
-
+if __name__ == '__main__':
+    main()
